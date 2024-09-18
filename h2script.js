7105d79c4f40def939252d45921e1cfee474ab5f
bln51000 <shanegibson1221@gmail.com>
2024-09-17 20:59:47 -0400

    Final Changes Made with Professor
    Got both buttons working at the same time by creating another file and getting rid of h2script import.

--- h2script.js
+++ h2script.js
@@ -1,7 +1,7 @@
 const button = document.getElementById('toggle-schedule');
 const div = document.getElementById('schedule-container');
 const addNameButton = document.getElementById('toggle-name');
-const changeNameButton = document.getElementById('heading-title');
+
 /*
 button.addEventListener('click', function() {
     if(button = document.getElementById('toggle-schedule'))
@@ -11,12 +11,7 @@ button.addEventListener('click', function() {
    });
 */
 
-addNameButton.addEventListener('click', function() {
-    // Prompt the user to input their name
-    const name = prompt('Please enter your name:');
-    
-    changeNameButton.textContent = name;
-   });
+
 
    button.addEventListener('click', function() {
     if(div.style.display == 'none')
@@ -31,5 +26,6 @@ addNameButton.addEventListener('click', function() {
     }
     
    });
-
+   
+   
    
